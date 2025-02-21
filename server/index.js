3ea70981cfe498bbc27ba0930dbede8297e08c0f
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2025-02-21 12:41:09 +0700

    tao route

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+import express from "express";
+import authRoutes from "./routes/auth.js";
+import todoRoutes from "./routes/todo.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use("/api/user", authRoutes);
+app.use("/api/todo", todoRoutes);
+
+app.get("/", (req, res, next) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
