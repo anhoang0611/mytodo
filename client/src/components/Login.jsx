1a97b3f0d54718d12e86b95c7a56c0b1dfaf98c0
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2025-02-23 06:39:58 +0700

    register user

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+//import components
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
+
 
 const Login = () => {
     return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="h-screen flex justify-center items-center -translate-y-16"> 
+        <form action="" className="flex flex-col gap-6 max-w-xl w-full">
+          <div className="flex flex-col gap-2">
+            <Label>Email</Label>
+            <Input type="email" name="email" placeholder="Enter email" />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label>Password</Label>
+            <Input type="password" name="password" placeholder="Enter password" />
+          </div>
+          <Button type="submit">Login</Button>
+          <span className="text-[#64748b] text-center">Don't have an account? <Link to="/register" className="transition ease-in-out hover:cursor-pointer hover:text-[#000] hover:underline">
+            Register
+          </Link></span>
+      </form>
     </div>
   );
 };
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState, useEffect, useFormState } from "react";
+//import components
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Link, useNavigate } from "react-router-dom";
+import { register } from "../actions/userAction";
 
 const Register = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [state, formAction, isPending] = useFormState(register,{
+    error: null,
+    success: null
+  });
+
+  useEffect(() => { 
+    if (state.success) { 
+      setTimeout(() => {
+        navigate("/login");
+      }, 2000);
+    }
+  }, [state.success])
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  
+
     return (
-    <div>
-      <h1>Register</h1>
+    <div className="h-screen flex justify-center items-center -translate-y-16"> 
+        <form action={formAction} className="flex flex-col gap-6 max-w-xl w-full">
+          <div className="flex flex-col gap-2">
+            <Label>Email</Label>
+            <Input type="email" name="email" placeholder="Enter email" value={formData.email}
+            onChange={handleChange}
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label>Password</Label>
+            <Input type="password" name="password" placeholder="Enter password" value={formData.password} onChange={handleChange} />
+          </div>
+          <Button disabled={isPending}>
+            {
+            isPending ? "Registering..." : "Register"
+            }
+          </Button>
+          <span className="text-[#64748b] text-center">Already have an account? <Link to="/login" className="transition ease-in-out hover:cursor-pointer hover:text-[#000] hover:underline">
+            Login
+          </Link></span>
+      </form>
     </div>
   );
 };
