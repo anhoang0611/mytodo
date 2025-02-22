c81401ff8a700f2770b11ad945ba046220f44f80
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2025-02-22 18:04:31 +0700

    config Shadcn and React-router-dom

---
+++ client/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Todos from './components/Todos.jsx'
+import Login from './components/Login.jsx'
+import Register from './components/Register.jsx'
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/todos" element={<Todos />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
