887f931262974ea8c2e320cef37ebfff1103c11d
anhoang0611 <94751169+anhoang0611@users.noreply.github.com>
2025-02-24 15:42:55 +0700

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Toaster } from 'react-hot-toast'
 import './index.css'
-import App from './App.jsx'
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Todos from './components/Todos.jsx'
 import Login from './components/Login.jsx'
